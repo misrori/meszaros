933ddfc19942191fb099acdf6e9ad3361c22a13a
misrori <ormraat.pte@gmail.com>
2017-05-02 10:33:04 +0200

    plot function

--- plot.R
+++ plot.R
@@ -1,12 +1,18 @@
 library(plotly)
-adat <- read.csv('ceg_infok.txt', sep = '\t')
-adat <- adat [adat$ceg_id==1,]
+ceg_info <- read.csv('ceg_infok.txt', sep = '\t')
 
+my_company_plot <- function(adat, ceg_id){
 
-p <- plot_ly(adat, x = ~ev, y = ~ado_elott, type = 'bar', name = 'ado_elott') %>%
-  add_trace(y = ~ado_utan, name = 'ado_utan') %>%
-  add_trace(y = ~merleg, name = 'merleg') %>%
-  layout(yaxis = list(title = 'Összeg'), barmode = 'group')
+  adatom <- adat[adat$ceg_id==ceg_id,]
+  
+  
+  p <- plot_ly(adatom, x = ~ev, y = ~ado_elott, type = 'bar', name = 'ado_elott') %>%
+    add_trace(y = ~ado_utan, name = 'ado_utan') %>%
+    add_trace(y = ~merleg, name = 'merleg') %>%
+    layout(yaxis = list(title = 'Összeg'), barmode = 'group')
+  
+  
+  return(p)
+}
 
-
-p
+my_company_plot(adat = ceg_info, ceg_id = 2)
