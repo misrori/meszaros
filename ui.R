190086f1c5ff89df366e8a19c6b1f4b687773b48
misrori <ormraat.pte@gmail.com>
2017-05-05 13:26:26 +0200

    forras

--- ui.R
+++ ui.R
@@ -31,7 +31,10 @@ navbarPage(
   tabPanel("Na de mennyit is keres a Mészáros?",
            br(),
            div(textOutput('my_timer'), align = "center"),
-           div(plotlyOutput("my_money_plotom", height = "800", width = "1200"), align = "center")
+           div(plotlyOutput("my_money_plotom", height = "800", width = "1200"), align = "center"),
+           tags$div(
+           HTML(' <center> <a target="_blank", href="http://index.hu/mindekozben/poszt/2017/04/27/meszaros_lorincnek_nem_eri_meg_lehajolni_egy_otezresert_mert_jobb_az_orabere/">Forrás</a> </center>')
+           )
   ),
   
   tags$head(
